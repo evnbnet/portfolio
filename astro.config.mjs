b04469964cb5ea6bdef92cfc473376f86ba1e468
Evan <evan.bennett@outlook.es>
2026-01-17 00:47:51 -0300

    change a few things

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://evnbnet.github.io',
-  base: '/portfolio/',
   integrations: [react()],
 
   vite: {
